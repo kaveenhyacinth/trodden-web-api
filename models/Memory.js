e35eacce5056c02ca65b2e33e5e363d2a622f5a5
kaveenhyacinth <alwiskaveen@gmail.com>
2021-02-16 08:44:34 +0530

    Add Memory schema model

---
+++ models/Memory.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const memorySchema = new mongoose.Schema(
+  {
+    content: String,
+    destination: String,
+    tags: [
+      {
+        type: ObjectId,
+        ref: "Tag",
+      },
+    ],
+    heats: [
+      {
+        type: ObjectId,
+        ref: "Nomad",
+      },
+    ],
+    comments: [
+      {
+        commentor: {
+          type: ObjectId,
+          ref: "Nomad",
+        },
+        content: String,
+      },
+    ],
+    images: [
+      {
+        data: Buffer,
+        contentType: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Memory = mongoose.model("Memory", memorySchema, "memories");
+
+module.exports = Memory;
