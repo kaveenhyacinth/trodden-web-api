50e57a8a798560afc96a4c9af2ddb7d82cf917c9
kaveenhyacinth <alwiskaveen@gmail.com>
2021-02-20 08:36:32 +0530

    Add caravan router

---
+++ routes/caravan.route.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middlewares/multerUploader");
+
+const {
+  isAuthenticated,
+  isSignedIn,
+} = require("../middlewares/authenticationChecker");
+const {
+  createCaravanController,
+  getCaravanByIdController,
+  getCaravansByOwnerController,
+  getCaravansByUserController,
+  connectToCaravanController,
+  updateCaravanController,
+  deleteCaravanController,
+} = require("../controllers/caravan.controller");
+
+router.get("/own", isSignedIn, isAuthenticated, getCaravansByOwnerController);
+router.get("/my", isSignedIn, isAuthenticated, getCaravansByUserController);
+router.get(
+  "/:caravanId",
+  isSignedIn,
+  isAuthenticated,
+  getCaravanByIdController
+);
+
+router.post(
+  "/new",
+  isSignedIn,
+  isAuthenticated,
+  upload.single("image"),
+  createCaravanController
+);
+
+router.patch(
+  "/join/:caravanId",
+  isSignedIn,
+  isAuthenticated,
+  connectToCaravanController
+);
+router.put(
+  "/update/:caravanId",
+  isSignedIn,
+  isAuthenticated,
+  updateCaravanController
+);
+
+router.delete(
+  "/delete/:caravanId",
+  isSignedIn,
+  isAuthenticated,
+  deleteCaravanController
+);
+
+module.exports = router;
