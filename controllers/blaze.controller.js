13aa57c1d6b7784cbc7d029a503a1d13968f8054
kaveenhyacinth <alwiskaveen@gmail.com>
2021-02-21 09:30:11 +0530

    Add minor fixes

---
+++ controllers/blaze.controller.js
@@ -0,0 +1,16 @@
+const {} = require("../services/CommunityService");
+
+// TODO: Create new blaze
+
+// TODO: Get blaze by id
+
+// TODO: Get blazes by caravan
+
+// TODO: Update a blaze
+
+// TODO: Mark as going to a blaze
+
+// TODO: Delete a blaze
+
+
+module.exports = {}
