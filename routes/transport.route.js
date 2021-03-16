91588af0780ecbf0888c9d0664408e844aa6f28e
kaveenhyacinth <alwiskaveen@gmail.com>
2021-03-16 11:14:35 +0530

    Minor fix

--- routes/transport.route.js
+++ routes/transport.route.js
@@ -8,7 +8,7 @@ const transport = require("../controllers/transport.controller");
 router.get("/image/:filename", transport.downloadImage);
 router.get("/video/:filename", transport.downloadVideo);
 
-router.post("/image/add", upload.single("images"), transport.uploadImage);
+router.post("/image/add", upload.single("image"), transport.uploadImage);
 router.post("/images/add", upload.array("images[]", 9), transport.uploadImages);
 router.post("/videos/add", upload.array("videos[]", 2), transport.uploadVideos);
 
